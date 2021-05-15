8b1916414e053f5a80ed392e3453895cc21996dd
Vanta Pretos <inbox.seenu@gmail.com>
2021-05-15 11:07:26 +0530

    Add databaseURL

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,16 @@
 import * as admin from 'firebase-admin';
 import request from 'request';
+import { CalenderResponse } from './dataModel';
 
 admin.initializeApp({
     credential: admin.credential.cert('./covin-notif-firebase-adminsdk-jv7hz-21fe98fa40.json'),
-    // databaseURL: 'https://covin-notify.firebaseio.com'
+    databaseURL: 'https://covin-notif-default-rtdb.firebaseio.com/'
 });
 
 const pincode = '600050';
 const date = '14-05-2021';
 
-request(`https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=${pincode}&date=${date}`, { json: true }, (err, res, body) => {
+request(`https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=${pincode}&date=${date}`, { json: true }, (err, res, body: CalenderResponse) => {
   if (err) { return console.log(err); }
-  console.log(body);
+  console.log(body.centers);
 });
