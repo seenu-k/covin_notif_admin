7328f122aae9c6670195c3bf95c69e28188fbd52
Vanta Pretos <inbox.seenu@gmail.com>
2021-05-27 08:13:25 +0530

    Use got instead of request

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 import * as admin from 'firebase-admin';
-import request from 'request';
+import got from 'got';
 import { CalenderResponse } from './dataModel';
 
 admin.initializeApp({
@@ -7,10 +8,39 @@ admin.initializeApp({
     databaseURL: 'https://covin-notif-default-rtdb.firebaseio.com/'
 });
 
-const pincode = '600050';
-const date = '14-05-2021';
+const dateObj = new Date();
+const date = `${dateObj.getDate().toLocaleString('en-IN', {minimumIntegerDigits: 2, useGrouping:false})}-${dateObj.getMonth().toLocaleString('en-IN', {minimumIntegerDigits: 2, useGrouping:false})}-${dateObj.getFullYear()}`;
 
-request(`https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=${pincode}&date=${date}`, { json: true }, (err, res, body: CalenderResponse) => {
-  if (err) { return console.log(err); }
-  console.log(body.centers);
+const pincodes: string[] = [];
+const districts: number[] = [];
+
+admin.database().ref('locations').on('child_added', (snapshot) => {
+  if(snapshot.key?.length===6) {
+    pincodes.push(snapshot.key);
+  }
+  else {
+    districts.push(Number(snapshot.key));
+  }
 });
+
+const getCentersByPincode = (pincode: string) => {
+  return got.get(`https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=${pincode}&date=${date}`).json().then((response) => {
+    return (response as CalenderResponse).centers;
+  }).catch(() => {
+    return null;
+  });
+};
+
+const getCentersByDistrict = (district_id: number) => {
+  return got.get(`https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=${district_id}&date=${date}`).json().then((response) => {
+    return (response as CalenderResponse).centers;
+  }).catch(() => {
+    return null;
+  });
+};
+
+const mainLoop = () => {
+  pincodes.map(pincode => getCentersByPincode(pincode));
+};
+
+setInterval(mainLoop, 10000);
